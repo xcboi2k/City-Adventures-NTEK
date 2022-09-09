0dd4ff9c2c6f6b7453ece1181174f808955ccda8
Gheian Agustin <gheianagustin2001@gmail.com>
2022-09-09 14:14:29 +0800

    collision on vehicle, score for coin

--- Assets/Walking/Scripts/CharacterColScript.cs
+++ Assets/Walking/Scripts/CharacterColScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterColScript : MonoBehaviour
 {
+    public int score;
+
     private void OnTriggerEnter(Collider Col)
     {
         if (Col.gameObject.tag == "PlayerPointA")
@@ -16,8 +18,24 @@ public class CharacterColScript : MonoBehaviour
             Debug.Log("You reached Point B.");
         }
 
-        if(Col.gameObject.tag == "Coin"){
+        if(Col.gameObject.tag == "Coin")
+        {
+            score += 1;
             Debug.Log("Coin added to score.");
+        }
+
+        if(Col.gameObject.tag == "Road")
+        {
+            score -= 1;
+            Debug.Log("Go back to Sidewalk!");
+        }
+
+        if(Col.gameObject.tag == "Gib")
+        {
+            score = 0;
+            Destroy(Col.gameObject);
+            Destroy(gameObject);
+            Debug.Log("You were hit by a car.");
         }
     }  
 }
